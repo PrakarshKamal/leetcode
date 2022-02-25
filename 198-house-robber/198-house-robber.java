a48c3bad7544ccee8a290fdb7ae5234fd0c4af90
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-02-25 17:14:47 -0500

    Time: 0 ms (100.00%), Space: 39.6 MB (42.87%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        int n = nums.length;
+        
+        int[] dp = new int [n];
+        
+        dp[0] = nums[0];
+        
+        if (n == 1) {
+            return dp[0];
+        }
+        
+        dp[1] = Math.max(nums[0], nums[1]);
+        
+        // iterating from 0 to ith element
+        // can also do from ith to n 
+        for (int i = 2; i < n; i++) {
+            
+            dp[i] = Math.max(
+                nums[i] + dp[i-2], //robbing
+                dp[i-1] // not robbing
+            );
+        }
+        return dp[n-1];
+    }
+}
