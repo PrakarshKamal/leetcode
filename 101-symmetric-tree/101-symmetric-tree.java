68a12cd76f0011d4b5789309c9c00d1614e442e1
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-03-06 04:44:57 -0500

    Time: 2 ms (12.67%), Space: 42.7 MB (12.15%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -13,27 +13,65 @@
  *     }
  * }
  */
+
+// // Recursive solution
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         if (root == null) {
+//             return true; 
+//         }
+//         else {
+//             return check(root.left, root.right);
+//         }
+//     }
+    
+//     private boolean check(TreeNode leftSubTree, TreeNode rightSubTree) {
+//          if (leftSubTree == null && rightSubTree == null) {
+//              return true;
+//          }
+        
+//         else if (leftSubTree == null || rightSubTree == null || leftSubTree.val != rightSubTree.val) {
+//             return false;
+//         }
+        
+//         else {
+//             return check(leftSubTree.left, rightSubTree.right) && check(leftSubTree.right, rightSubTree.left);
+//         }
+//     }
+// }
+
+// Iterative solution
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
-            return true; 
-        }
-        else {
-            return check(root.left, root.right);
-        }
-    }
-    
-    private boolean check(TreeNode leftSubTree, TreeNode rightSubTree) {
-         if (leftSubTree == null && rightSubTree == null) {
-             return true;
-         }
         
-        else if (leftSubTree == null || rightSubTree == null || leftSubTree.val != rightSubTree.val) {
-            return false;
-        }
+        if (root == null) return true;
         
-        else {
-            return check(leftSubTree.left, rightSubTree.right) && check(leftSubTree.right, rightSubTree.left);
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        q.add(root.right);
+        q.add(root.left);
+        
+        while (!q.isEmpty()) {
+            
+            TreeNode leftSubTree = q.remove();
+            TreeNode rightSubTree = q.remove();
+            
+            if (leftSubTree == null && rightSubTree == null) {
+                continue;
+            }
+            
+            if (leftSubTree == null || rightSubTree == null || leftSubTree.val != rightSubTree.val) {
+                return false;
+            }
+            
+            else {
+                q.add(leftSubTree.left);
+                q.add(rightSubTree.right);
+                
+                q.add(leftSubTree.right);
+                q.add(rightSubTree.left);
+            }
         }
+        return true;
     }
 }
