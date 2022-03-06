08fe9dae7ca40f1d940f4d461737f218eb516ece
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-03-06 09:40:10 -0500

    Time: 1 ms (24.30%), Space: 43.2 MB (29.97%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -13,16 +13,57 @@
  *     }
  * }
  */
+// class Solution {
+//     public int maxDepth(TreeNode root) {
+        
+//         if (root == null) {
+//             return 0;
+//         }
+//         else {
+//             int l = maxDepth(root.left);
+//             int r = maxDepth(root.right);
+//             return Math.max(l, r) + 1;
+//         }
+//     }
+// }
+
 class Solution {
     public int maxDepth(TreeNode root) {
-        
         if (root == null) {
             return 0;
         }
-        else {
-            int l = maxDepth(root.left);
-            int r = maxDepth(root.right);
-            return Math.max(l, r) + 1;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        int level = 1;
+        int maxD = 0;
+        
+        q.add(root);
+        
+        while (!q.isEmpty()) {
+            
+            int s = q.size();
+            
+            for (int i = 0; i < s; i++) {
+                
+                TreeNode curr = q.remove();
+                
+                if (curr.left == null && curr.right == null) {
+                    maxD = Math.max(maxD, level);
+                }
+                
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+            
+            level++;
         }
+        
+        return maxD;
     }
 }
