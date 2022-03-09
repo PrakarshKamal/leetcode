b026fdc9dcaa5ff0dae9e0c184059595134712c4
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-03-09 00:03:46 -0500

    Time: 8 ms (38.34%), Space: 44.6 MB (52.14%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,39 @@
+class Solution {
+    
+    private List<List<Integer>> ans;
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        ans = new ArrayList<>();
+        
+        comboSum(candidates, 0, target, new ArrayList<>());
+        
+        return ans;
+    }
+    
+    private void comboSum(int[] candidates, int index, int target, List<Integer> path) {
+        
+        // base case
+        
+        if (target < 0 || index == candidates.length) {
+            return;
+        }
+        
+        if (target == 0) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        
+        // choose index
+        path.add(candidates[index]);
+        
+        // recurse
+        comboSum(candidates, index, target-candidates[index], path);
+        
+        // backtrack
+        path.remove(path.size()-1);
+        
+        // not choosing index
+        comboSum(candidates, index+1, target, path);
+    }
+}
