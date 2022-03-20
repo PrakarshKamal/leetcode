30e3af364db4d2c1f8bdedec252697449f606398
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-03-20 05:25:40 -0400

    Time: 1 ms (81.74%), Space: 43.4 MB (25.82%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,5 +1,27 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        long N = n;
+        
+        if (N < 0) {
+            x = 1/x;
+            N = -N;
+        }
+        return myPowRecursive(x, N);
+    }
+    
+    private double myPowRecursive(double x, long n) {
+        
+        // base
+        if (n == 0) {
+            return 1;
+        }
+        
+        // rec
+        double res = myPowRecursive(x, n/2);
+        
+        if (n % 2 == 0) {
+            return res * res;
+        }
+        return res * res * x;
     }
 }
