e6efbdd82350cc844b65240a1258415d0047d795
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-04-03 18:27:43 -0400

    Time: 8 ms (58.07%), Space: 49.2 MB (31.63%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        int[] indegree = new int[numCourses];
+        
+        for (int[] prereq : prerequisites) {
+            
+            int dependent = prereq[0];
+            int source = prereq[1];
+            
+            if(!adjList.containsKey(source)) {
+                adjList.put(source, new ArrayList<>());
+            }
+            
+            adjList.get(source).add(dependent);
+            
+            ++indegree[dependent];
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        
+        int completedCourses = 0;
+        
+        while (!q.isEmpty()) {
+            
+            int course = q.remove();
+            ++completedCourses;
+            
+            List<Integer> dependents = adjList.get(course);
+            
+            if (dependents != null) {
+                for (int depend : dependents) {
+                    indegree[depend]--;
+                    
+                    if (indegree[depend] == 0) {
+                        q.add(depend);
+                    }
+                }
+            }
+        }
+        
+        return completedCourses == numCourses;
+    }
+}
