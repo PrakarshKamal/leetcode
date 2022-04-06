824f97c65080f624ddcb3c1a1848908142cd8d20
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-04-06 00:10:56 -0400

    Time: 3 ms (6.07%), Space: 53.1 MB (5.08%) - LeetHub

---
+++ 275-h-index-ii/275-h-index-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+    public int hIndex(int[] citations) {
+        
+        Arrays.sort(citations);
+        
+        int n = citations.length;
+        
+        int h = n;
+        
+        while (h > 0) {
+            
+            if(citations[n-h] >= h) {
+                return h;
+            }
+            
+            h--;
+        }
+        
+        return h;
+    }
+}
