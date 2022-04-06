9668aa6564b9c684c25c27ff7c9af87d8ded04da
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-04-06 00:32:24 -0400

    Time: 0 ms (100.00%), Space: 40 MB (96.03%) - LeetHub

--- 274-h-index/274-h-index.java
+++ 274-h-index/274-h-index.java
@@ -1,21 +1,36 @@
 class Solution {
     public int hIndex(int[] citations) {
         
-        Arrays.sort(citations);
+        // Arrays.sort(citations);
         
         int n = citations.length;
+
+        int[] counts = new int[n+1];
+        
+        for (int c : citations) {
+            if (c >= n) {
+                counts[n]++;
+            }
+            else {
+                counts[c]++;
+            }
+        }
+        
+        for (int i = n-1; i >= 0; i--) {
+            counts[i] += counts[i+1];
+        }
         
         int h = n;
         
         while (h > 0) {
             
-            if(citations[n-h] >= h) {
+            if(counts[h] >= h) {
                 return h;
             }
             
             h--;
         }
         
-        return h;
+        return 0;
     }
 } 
