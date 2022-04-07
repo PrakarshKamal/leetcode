10f5ed86e068b3d6d1f777376c03743f79fb995a
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-04-06 23:36:50 -0400

    Time: 1 ms (81.61%), Space: 63.9 MB (77.23%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,17 +1,49 @@
 class Solution {
+    
+    //TC: O(2n)
+    //SC: O(1)
+    
     public void rotate(int[] nums, int k) {
+        int n = nums.length;
         
-        Queue<Integer> q = new LinkedList<>();
+        k = k % n;
         
-        k = k % nums.length;
+        // 0 1 2 3 4 5
+        reverse(nums, 0, n-1);
+        // 5 4 3 2 1 0
         
-        for (int i = nums.length - k; i < nums.length; i++) {
-            q.add(nums[i]);
-        }
+        // reversing first 3 elements when k = 3
+        reverse(nums, 0, k - 1);
         
-        for (int j = 0; j < nums.length; j++) { 
-            q.add(nums[j]);
-            nums[j] = q.remove();
+        reverse(nums, k, n-1);
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            
+            start++;
+            end--;
         }
     }
+    
+//     public void rotate(int[] nums, int k) {
+        
+//         Queue<Integer> q = new LinkedList<>();
+        
+//         k = k % nums.length;
+        
+//         for (int i = nums.length - k; i < nums.length; i++) {
+//             q.add(nums[i]);
+//         }
+        
+//         for (int j = 0; j < nums.length; j++) { 
+//             q.add(nums[j]);
+//             nums[j] = q.remove();
+//         }
+//     }
+    
 }
