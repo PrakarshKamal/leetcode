fe4749b103c79c21732a5ca942d5321f16271b39
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-04-10 10:59:30 -0400

    Time: 9 ms (45.62%), Space: 43.9 MB (65.62%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,7 +1,14 @@
+//TC: O(n) for looping through tokens
+//SCL O(n) for maintaining stack
+
 class Solution {
     public int evalRPN(String[] tokens) {
         
-        //["2","1","+","3","*"]
+        //approach is to keep pushing integers until we see an operator (+, -, *, /)
+        //when operator is seen, pop twice from stack and evaluate
+        //in evaluation, we place second operand before first (b+a, b-a, b*a, b/a) to maintain the order
+        //then push the result of evaluation to stack
+        //finally return top of stack
         
         Stack<Integer> stack = new Stack<>();
         
