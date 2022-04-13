88fac408fdfddf0ec6ef8911bcf3ef2db338747a
Prakarsh Kamal <57968205+PrakarshKamal@users.noreply.github.com>
2022-04-12 23:56:15 -0400

    Time: 1 ms (98.80%), Space: 39.4 MB (89.78%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 1;
+        int end = x;
+        
+        if (x == 0) {
+            return 0;
+        }
+        
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            
+            if (mid <= x / mid && (mid+1) > x / (mid + 1)) {
+                return mid;
+            }
+            else if (mid > x / mid) {
+                end = mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
