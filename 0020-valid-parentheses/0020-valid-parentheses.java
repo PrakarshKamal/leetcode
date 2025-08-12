5f05764ede748890682e6f43112b24177183d313
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-08-11 22:04:17 -0400

    Time: 1 ms (99.42%), Space: 40.4 MB (100%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,48 @@
+//TC: O(n) for looping through s
+//SC: O(n) for stack of size length of given s
+
 class Solution {
+    
+    Stack<Character> stack;
+    
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        if(s.length() == 1) {
+        stack = new Stack<>();
+        
+        // base case checking if string length is odd
+        // if odd, not valid
+        if (s.length() % 2 != 0) {
             return false;
         }
-        for(char c : s.toCharArray()) {
-            if(c == '(' || c == '[' || c == '{') {
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
             }
+            
             else {
-                if(stack.isEmpty()) {
-                    return false;
+                
+                if (stack.isEmpty()) {
+                    return false;    
                 }
-                char top = stack.pop();
-                if (c == ')' && top != '(' ||
-                    c == ']' && top != '[' ||
-                    c == '}' && top != '{') {
+                
+                else if (isValidParentheses(stack.peek(), c) == true) {
+                    stack.pop();
+                }
+                else {
                     return false;
                 }
             }
         }
         return stack.isEmpty();
+    }
+    
+    private boolean isValidParentheses(char brackOpen, char brackClose) {
+        return  brackOpen == '(' && brackClose == ')'
+                                ||
+                brackOpen == '{' && brackClose == '}'
+                                ||
+                brackOpen == '[' && brackClose == ']';
     }
 }
