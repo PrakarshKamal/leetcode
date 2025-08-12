b14745352d57414c3046d87c81453be0e8ec6126
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-08-11 22:02:58 -0400

    Time: 134 ms (5.72%), Space: 41.7 MB (66.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,37 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        // if(s.length() == 1) {
-        //     return false;
+        // Stack<Character> stack = new Stack<>();
+        // // if(s.length() == 1) {
+        // //     return false;
+        // // }
+        // for(char c : s.toCharArray()) {
+        //     if(c == '(' || c == '[' || c == '{') {
+        //         stack.push(c);
+        //     }
+        //     else {
+        //         if(stack.isEmpty()) {
+        //             return false;
+        //         }
+        //         char top = stack.pop();
+        //         if (c == ')' && top != '(' ||
+        //             c == ']' && top != '[' ||
+        //             c == '}' && top != '{') {
+        //             return false;
+        //         }
+        //     }
         // }
-        for(char c : s.toCharArray()) {
-            if(c == '(' || c == '[' || c == '{') {
-                stack.push(c);
+        // return stack.isEmpty();
+        Stack<Character> stack = new Stack();
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.push(map.get(c));
             }
-            else {
-                if(stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if (c == ')' && top != '(' ||
-                    c == ']' && top != '[' ||
-                    c == '}' && top != '{') {
-                    return false;
-                }
-            }
+            else if (stack.empty() || stack.pop() != c) return false;
         }
-        return stack.isEmpty();
+        return stack.empty();
     }
 }
