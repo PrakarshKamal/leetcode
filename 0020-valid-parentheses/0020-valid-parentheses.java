436c569b4d38dc316fa09cd453f1b19749c727be
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-08-11 21:54:27 -0400

    Time: 1 ms (99.42%), Space: 37.1 MB (100%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> myStack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                myStack.push(c);
+            } else if (c == ')' && !myStack.isEmpty() && myStack.peek() == '(') {
+                myStack.pop();
+            } else if (c == '}' && !myStack.isEmpty() && myStack.peek() == '{') {
+                myStack.pop();
+            } else if (c == ']' && !myStack.isEmpty() && myStack.peek() == '[') {
+                myStack.pop();
+            } else {
+                return false;
+            }
+        }
+        return myStack.isEmpty();
+    }
+}
