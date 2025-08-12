2233ad6b5d90c6f3dd5d131a26946513bb3a608b
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-08-11 22:05:08 -0400

    Time: 115 ms (5.72%), Space: 41.7 MB (66.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,70 +1,25 @@
-// class Solution {
-//     public boolean isValid(String s) {
-//         Stack<Character> stack = new Stack<>();
-//         if(s.length() == 1) {
-//             return false;
-//         }
-//         for(char c : s.toCharArray()) {
-//             if(c == '(' || c == '[' || c == '{') {
-//                 stack.push(c);
-//             }
-//             else {
-//                 if(stack.isEmpty()) {
-//                     return false;
-//                 }
-//                 char top = stack.pop();
-//                 if (c == ')' && top != '(' ||
-//                     c == ']' && top != '[' ||
-//                     c == '}' && top != '{') {
-//                     return false;
-//                 }
-//             }
-//         }
-//         return stack.isEmpty();
-//     }
-// }
 class Solution {
-    
-    
-    
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        
-        // base case checking if string length is odd
-        // if odd, not valid
-        if (s.length() % 2 != 0) {
+        if(s.length() == 1) {
             return false;
         }
-        
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            
-            if (c == '(' || c == '[' || c == '{') {
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '[' || c == '{') {
                 stack.push(c);
             }
-            
             else {
-                
-                if (stack.isEmpty()) {
-                    return false;    
+                if(stack.isEmpty()) {
+                    return false;
                 }
-                
-                else if (isValidParentheses(stack.peek(), c) == true) {
-                    stack.pop();
-                }
-                else {
+                char top = stack.pop();
+                if (c == ')' && top != '(' ||
+                    c == ']' && top != '[' ||
+                    c == '}' && top != '{') {
                     return false;
                 }
             }
         }
         return stack.isEmpty();
-    }
-    
-    private boolean isValidParentheses(char brackOpen, char brackClose) {
-        return  brackOpen == '(' && brackClose == ')'
-                                ||
-                brackOpen == '{' && brackClose == '}'
-                                ||
-                brackOpen == '[' && brackClose == ']';
     }
 }
