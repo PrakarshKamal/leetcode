3de72b95a6084d90949d0a22931313cf2064d2d8
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-08-11 22:03:12 -0400

    Time: 137 ms (5.72%), Space: 41.5 MB (92.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,37 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        // Stack<Character> stack = new Stack<>();
-        // // if(s.length() == 1) {
-        // //     return false;
-        // // }
-        // for(char c : s.toCharArray()) {
-        //     if(c == '(' || c == '[' || c == '{') {
-        //         stack.push(c);
-        //     }
-        //     else {
-        //         if(stack.isEmpty()) {
-        //             return false;
-        //         }
-        //         char top = stack.pop();
-        //         if (c == ')' && top != '(' ||
-        //             c == ']' && top != '[' ||
-        //             c == '}' && top != '{') {
-        //             return false;
-        //         }
-        //     }
-        // }
-        // return stack.isEmpty();
-        Stack<Character> stack = new Stack();
-        Map<Character, Character> map = new HashMap<>();
-        map.put('(', ')');
-        map.put('[', ']');
-        map.put('{', '}');
-        for (char c : s.toCharArray()) {
-            if (map.containsKey(c)) {
-                stack.push(map.get(c));
-            }
-            else if (stack.empty() || stack.pop() != c) return false;
+        Stack<Character> stack = new Stack<>();
+        if(s.length() == 1) {
+            return false;
         }
-        return stack.empty();
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(' ||
+                    c == ']' && top != '[' ||
+                    c == '}' && top != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
