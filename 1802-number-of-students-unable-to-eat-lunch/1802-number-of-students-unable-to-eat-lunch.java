527b312e0e3919a49b4f55c8bfc30c0f44d897f8
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-08-18 21:55:15 -0400

    Time: 0 ms (100%), Space: 41.2 MB (94.42%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.java
@@ -1,25 +1,57 @@
 class Solution {
     // Using Queue, O(n^2) worst case, O(n) space
     public int countStudents(int[] students, int[] sandwiches) {
-        Queue<Integer> q = new LinkedList<>();
+        // Queue<Integer> q = new LinkedList<>();
+
+        // for (int student : students) {
+        //     q.offer(student);
+        // }
+
+        // int count = 0;
+        // int i = 0;
+        // while (!q.isEmpty() && count < q.size()) {
+        //     if (q.peek() == sandwiches[i]) {
+        //         q.poll();
+        //         i++;
+        //         count = 0;
+        //     }
+        //     else {
+        //         q.offer(q.poll());
+        //         count++;
+        //     }
+        // }
+        // return q.size();
+
+        int zeroCount = 0;
+        int oneCount = 0;
 
         for (int student : students) {
-            q.offer(student);
-        }
-
-        int count = 0;
-        int i = 0;
-        while (!q.isEmpty() && count < q.size()) {
-            if (q.peek() == sandwiches[i]) {
-                q.poll();
-                i++;
-                count = 0;
+            if (student == 0) {
+                zeroCount++;
             }
             else {
-                q.offer(q.poll());
-                count++;
+                oneCount++;
             }
         }
-        return q.size();
+
+        for (int sandwich : sandwiches) {
+            if (sandwich == 0) {
+                if (zeroCount == 0) {
+                    return oneCount;
+                }
+                else {
+                    zeroCount--;
+                }
+            }
+            else {
+                if (oneCount == 0) {
+                    return zeroCount;
+                }
+                else {
+                    oneCount--;
+                }
+            }
+        }
+        return 0;
     }
 }
