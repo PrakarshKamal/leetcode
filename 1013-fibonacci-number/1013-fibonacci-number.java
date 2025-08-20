42cb677730e7fc2da1cb48b16452a4a2dbc0f9a5
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-08-20 00:50:50 -0400

    Time: 8 ms (45.39%), Space: 40.4 MB (63.81%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        // Recursion base case
+        if (n <= 1) {
+            return n;
+        }
+        int fib1 = fib(n - 1);
+        int fib2 = fib(n - 2);
+
+        return fib1 + fib2;
+    }
+}
