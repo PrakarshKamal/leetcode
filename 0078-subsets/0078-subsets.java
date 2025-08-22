2354762a99ed3e812aaa7b9241fec207c8bf2c66
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-08-22 16:39:28 -0400

    Time: 1 ms (48.95%), Space: 42.5 MB (93.45%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        List<Integer> temp = new ArrayList<>();
+
+        backtrack(ans, temp, nums, 0);
+
+        return ans;
+    }
+
+    public void backtrack(List<List<Integer>> ans, List<Integer> temp, int[] nums, int start) {
+        // base case, add empty subset first
+        if (start == nums.length) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        // we include the number
+        temp.add(nums[start]);
+
+        backtrack(ans, temp, nums, start + 1);
+
+        // we don't include the number
+        temp.remove(temp.size() - 1);
+
+        backtrack(ans, temp, nums, start + 1);
+    }
+}
