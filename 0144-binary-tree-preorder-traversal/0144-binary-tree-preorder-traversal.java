759e02536ed748b6d0bd6b6d193d80b6efa10101
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-08-27 17:37:02 -0400

    Time: 0 ms (100%), Space: 41.6 MB (67.02%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -16,34 +16,35 @@
 class Solution {
     // Recursive O(n) time, O(n) space
     List<Integer> ans;
-    // public List<Integer> preorderTraversal(TreeNode root) {
-    //     ans = new ArrayList<>();
-    //     preorder(root);
-    //     return ans;
-    // }
-
-    // private void preorder(TreeNode root) {
-    //     if (root == null) {
-    //         return;
-    //     }
-    //     ans.add(root.val);
-    //     preorder(root.left);
-    //     preorder(root.right);
-    // }
-
-     public List<Integer> preorderTraversal(TreeNode root) {
+    public List<Integer> preorderTraversal(TreeNode root) {
         ans = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-
-        while (root != null || !stack.isEmpty()) {
-            while (root != null) {
-                stack.push(root);
-                ans.add(root.val);
-                root = root.left;
-            }
-            root = stack.pop();
-            root = root.right;
-        }
+        preorder(root);
         return ans;
-     }
+    }
+
+    private void preorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        ans.add(root.val);
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    // Iterative O(n) time, O(n) space
+    //  public List<Integer> preorderTraversal(TreeNode root) {
+    //     ans = new ArrayList<>();
+    //     Stack<TreeNode> stack = new Stack<>();
+
+    //     while (root != null || !stack.isEmpty()) {
+    //         while (root != null) {
+    //             stack.push(root);
+    //             ans.add(root.val);
+    //             root = root.left;
+    //         }
+    //         root = stack.pop();
+    //         root = root.right;
+    //     }
+    //     return ans;
+    //  }
 }
