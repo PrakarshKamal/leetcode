40948de81be1437505f91cdeba306f2b2779afb2
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-08-29 21:10:03 -0400

    Time: 0 ms (100%), Space: 41.4 MB (36.14%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,18 +15,44 @@
  */
 class Solution {
     // Recursive DFS O(n) time, O(n) space
+    // public TreeNode invertTree(TreeNode root) {
+    //     if (root == null) {
+    //         return root;
+    //     }
+        
+    //     TreeNode temp = root.left;
+    //     root.left = root.right;
+    //     root.right = temp;
+
+    //     invertTree(root.left);
+    //     invertTree(root.right);
+
+    //     return root;
+    // }
+
+    // BFS O(n) time, O(n) space
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return root;
         }
-        
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
 
-        invertTree(root.left);
-        invertTree(root.right);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
 
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+
+            TreeNode temp = curr.left;
+            curr.left = curr.right;
+            curr.right = temp;
+            
+            if (curr.left != null) {
+                q.offer(curr.left);   
+            }
+            if (curr.right != null) {
+                q.offer(curr.right);
+            }
+        }
         return root;
     }
 }
