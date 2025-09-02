01488072bf7b06cd0e28973695ec3d0b1af788c2
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-02 05:03:41 -0400

    Time: 1 ms (98.45%), Space: 41.8 MB (7.91%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.java
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -1,26 +1,50 @@
 class Solution {
-    // Brute force sorting approach, O(n log n) time, O(n) space
+    // Brute force sorting approach, O(n^2 log n) time, O(n) space
+    // public int lastStoneWeight(int[] stones) {
+    //     List<Integer> arr = new ArrayList<>();
+    //     for (int stone : stones) {
+    //         arr.add(stone);
+    //     }
+
+    //     while (arr.size() > 1) {
+    //         Collections.sort(arr, Collections.reverseOrder());
+
+    //         int y = arr.remove(0); // largest
+    //         int x = arr.remove(0); // next largest
+    //         int curr = y - x;
+
+    //         if (curr != 0) {
+    //             arr.add(curr);
+    //         }
+    //     }
+        
+    //     if (arr.isEmpty()) {
+    //         return 0;
+    //     }
+    //     return arr.get(0);
+    // }
+
     public int lastStoneWeight(int[] stones) {
-        List<Integer> arr = new ArrayList<>();
+        // Max heap approach, O(n log n) time, O(n) space
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
         for (int stone : stones) {
-            arr.add(stone);
+            maxHeap.offer(stone);
         }
 
-        while (arr.size() > 1) {
-            Collections.sort(arr, Collections.reverseOrder());
-
-            int y = arr.remove(0); // largest
-            int x = arr.remove(0); // next largest
+        while (maxHeap.size() > 1) {
+            int y = maxHeap.poll(); // largest
+            int x = maxHeap.poll(); // next largest
             int curr = y - x;
 
             if (curr != 0) {
-                arr.add(curr);
+                maxHeap.offer(curr);
             }
         }
-        
-        if (arr.isEmpty()) {
+
+        if (maxHeap.isEmpty()) {
             return 0;
         }
-        return arr.get(0);
+        return maxHeap.peek();
     }
 }
