8d97c91cd63e468412a3bdb360ad3e1f07e0eaa7
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-02 03:06:11 -0400

    Time: 2670 ms (5.02%), Space: 55.5 MB (5%) - LeetHub

---
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
@@ -0,0 +1,34 @@
+class KthLargest {
+
+    // Brute force with sorting
+    // O(n log n) time, O(n) space
+    List<Integer> ans;
+    int K;
+
+    public KthLargest(int k, int[] nums) {
+        ans = new ArrayList<>();
+        K = k;
+
+        for (int num : nums) {
+            ans.add(num);
+        }
+    }
+    
+    public int add(int val) {
+        // We add val to arr and sort
+        ans.add(val);
+        // Sort in reverse order (descending) meaning kth largest would be in beginning of array
+        Collections.sort(ans, Collections.reverseOrder());
+        return ans.get(K - 1); // index would be K - 1 since 0-indexed
+
+        // if we did ascending order
+        // Collections.sort(arr);
+        // return arr.get(arr.size() - 1) since kth largest is now at end of array
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
