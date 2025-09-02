b2de595df5034f82bbd527cbe11e8eff9e74d122
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-02 03:31:48 -0400

    Time: 22 ms (25.5%), Space: 49.9 MB (79.8%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
@@ -1,29 +1,56 @@
 class KthLargest {
 
-    // Brute force with sorting
-    // O(n log n) time, O(n) space
-    List<Integer> ans;
+    // // Brute force with sorting
+    // // O(n log n) time, O(n) space
+    // List<Integer> ans;
+    // int K;
+
+    // public KthLargest(int k, int[] nums) {
+    //     ans = new ArrayList<>();
+    //     K = k;
+
+    //     for (int num : nums) {
+    //         ans.add(num);
+    //     }
+    // }
+    
+    // public int add(int val) {
+    //     // We add val to arr and sort
+    //     ans.add(val);
+    //     // Sort in reverse order (descending) meaning kth largest would be in beginning of array
+    //     Collections.sort(ans, Collections.reverseOrder());
+    //     return ans.get(K - 1); // index would be K - 1 since 0-indexed
+
+    //     // if we did ascending order
+    //     // Collections.sort(arr);
+    //     // return arr.get(arr.size() - 1) since kth largest is now at end of array
+    // }
+
+    // Min Heap approach O(n log k) time for building heap, O(k) space since we store at most k elements in min heap
+    PriorityQueue<Integer> minHeap;
     int K;
 
     public KthLargest(int k, int[] nums) {
-        ans = new ArrayList<>();
         K = k;
+        minHeap = new PriorityQueue<>();
 
         for (int num : nums) {
-            ans.add(num);
+            minHeap.offer(num);
+
+            if (minHeap.size() > K) {
+                minHeap.poll();
+            }
         }
     }
-    
-    public int add(int val) {
-        // We add val to arr and sort
-        ans.add(val);
-        // Sort in reverse order (descending) meaning kth largest would be in beginning of array
-        Collections.sort(ans, Collections.reverseOrder());
-        return ans.get(K - 1); // index would be K - 1 since 0-indexed
 
-        // if we did ascending order
-        // Collections.sort(arr);
-        // return arr.get(arr.size() - 1) since kth largest is now at end of array
+    public int add(int val) {
+        minHeap.offer(val);
+
+        if (minHeap.size() > K) {
+            minHeap.poll();
+        }
+
+        return minHeap.peek();
     }
 }
 
