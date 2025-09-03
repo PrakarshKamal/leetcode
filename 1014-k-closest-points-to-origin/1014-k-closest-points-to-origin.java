732b54459d7f47cf6cd871ea9121e698dd25afd7
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-03 02:59:48 -0400

    Time: 32 ms (43.9%), Space: 50.9 MB (74.61%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,21 +1,31 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int[][] ans = new int[k][2];
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a,b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1])
-        );
-        
-        for(int[] point : points) {
-            pq.add(point);
-            
-            if (pq.size() > k) {
-                pq.remove();
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> Integer.compare(distance(b), distance(a))); 
+
+        if (k == points.length) {
+            return points;
+        }
+        for (int[] point : points) {
+            maxHeap.offer(point);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
             }
         }
-        
-        while (k-- > 0) {
-            ans[k] = pq.remove();
+
+        int[][] ans = new int[maxHeap.size()][2];
+        int i = 0;
+        while (!maxHeap.isEmpty()) {
+            int[] point = maxHeap.poll();
+            ans[i] = point;
+            i++;
         }
         return ans;
+    }
+
+    private int distance(int[] point) {
+        int x = point[0];
+        int y = point[1];
+        int euclid = x * x + y * y;
+        return euclid;
     }
 }
