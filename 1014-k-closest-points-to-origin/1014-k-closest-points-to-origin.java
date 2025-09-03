579889e4e29f1f9035afb1a62c150f710868222a
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-03 03:01:57 -0400

    Time: 31 ms (56.02%), Space: 51 MB (70.86%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,19 +1,25 @@
 class Solution {
+    // Max heap approach O(n log k) time, O(k) space
     public int[][] kClosest(int[][] points, int k) {
+        // We have to use Integer.compare as we have int[] in heap
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> Integer.compare(distance(b), distance(a))); 
 
+        //
         if (k == points.length) {
             return points;
         }
+        
+        // Build max heap 
         for (int[] point : points) {
             maxHeap.offer(point);
-            if (maxHeap.size() > k) {
+            if (maxHeap.size() > k) { // only maintain at most k points
                 maxHeap.poll();
             }
         }
 
         int[][] ans = new int[maxHeap.size()][2];
         int i = 0;
+        // build answer by polling the heap and setting to ans
         while (!maxHeap.isEmpty()) {
             int[] point = maxHeap.poll();
             ans[i] = point;
@@ -22,6 +28,7 @@ class Solution {
         return ans;
     }
 
+    // helper for computing distance, don't need to sqrt
     private int distance(int[] point) {
         int x = point[0];
         int y = point[1];
