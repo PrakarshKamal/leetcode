d292497ae6fae87a84f2579c400ce086d9ae11ef
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-10 10:55:59 -0400

    Time: 2 ms (94.03%), Space: 46.8 MB (52.26%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,19 +1,32 @@
 class Solution {
     // O(n log n ) time, O(n) space
-    public int[] countBits(int n) {
-        int[] ans = new int[n + 1];
+    // public int[] countBits(int n) {
+    //     int[] ans = new int[n + 1];
         
-        for (int i = 0; i <= n; i++) {
-            int count = 0;
-            int x = i;
-            while (x > 0) {
-                if ((x & 1) == 1) {
-                    count++;
-                }
-                x >>= 1;
+    //     for (int i = 0; i <= n; i++) {
+    //         int count = 0;
+    //         int x = i;
+    //         while (x > 0) {
+    //             if ((x & 1) == 1) {
+    //                 count++;
+    //             }
+    //             x >>= 1;
+    //         }
+    //         ans[i] = count;
+    //     }
+    //     return ans;
+    // }
+
+    // DP approach O(n) time, O(n) space
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+        int offset = 1;
+        for (int i = 1; i <= n; i++) {
+            if (offset * 2 == i) {
+                offset = i;
             }
-            ans[i] = count;
+            dp[i] = 1 + dp[i - offset];
         }
-        return ans;
+        return dp;
     }
 }
