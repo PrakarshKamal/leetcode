5602a72d79e0cdefd0a42b8e682f103b5136e29a
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-10 11:59:14 -0400

    Time: 1 ms (99.85%), Space: 45.9 MB (82.27%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    // O(1) time, O(1) space
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+}
