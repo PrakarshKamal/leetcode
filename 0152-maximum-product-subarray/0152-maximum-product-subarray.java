82d829e35ffb2346359cec79d71299688ad32416
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-11 15:43:10 -0400

    Time: 2 ms (54.92%), Space: 45.1 MB (32.19%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,13 +1,17 @@
 class Solution {
     // Kadane's algo, O(n) time, O(1) space
     public int maxProduct(int[] nums) {
-        int maxProd = nums[0];
-        int currMax = nums[0];
-        int currMin = nums[0];
+        int maxProd = nums[0]; // global max prod
+        int currMax = nums[0]; // max product ending at the current index
+        int currMin = nums[0]; // min product ending at the current index
         for (int i = 1; i < nums.length; i++) {
+            // we perform swap since currMax could
+            // become new currMin if we multiply by negative num
+            // or currMin could become new currMax if we multiply       
+            // 2 negatives together to get larger positive
             if (nums[i] < 0) {
-                int temp = currMax;
-                currMax = currMin;
+                int temp = currMax; 
+                currMax = currMin; 
                 currMin = temp;
             }
             currMax = Math.max(nums[i], nums[i] * currMax);
