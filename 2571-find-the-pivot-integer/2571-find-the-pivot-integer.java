1970ea0e9f5fec00cb1f99e7e694a607698645bb
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-17 20:01:43 -0400

    Time: 0 ms (100%), Space: 40.4 MB (91.88%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     public int pivotInteger(int n) {
-        // total sum 1 to n = n(n+1) / 2
+        // total sum 1 to n is n(n+1) / 2 = x ^ 2
         int total = n * (n + 1) / 2;
 
         int root = (int) Math.sqrt(total);
