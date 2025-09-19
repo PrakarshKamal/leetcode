c45532c65a8fa464751ceb97cb08a0dc25049695
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-19 02:18:51 -0400

    Time: 0 ms (100%), Space: 49.1 MB (62.67%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,24 @@
 class Solution {
     // Using Stack O(n) time, O(n) space
+    // public void reverseString(char[] s) {
+    //     Stack<Character> stack = new Stack<>();
+    //     for (char c : s) {
+    //         stack.push(c);
+    //     }
+    //     for (int i = 0; i < s.length; i++) {
+    //         s[i] = stack.pop();
+    //     }  
+    // }
     public void reverseString(char[] s) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : s) {
-            stack.push(c);
+        // Two Pointers O(n) time, O(1) space
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
         }
-        for (int i = 0; i < s.length; i++) {
-            s[i] = stack.pop();
-        }
-        
     }
 }
