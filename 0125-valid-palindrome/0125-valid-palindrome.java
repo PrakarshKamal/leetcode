6c673068a4fae089fdfb82e76f094bf245da2ebc
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-19 03:03:27 -0400

    Time: 6 ms (45.93%), Space: 57.4 MB (5.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,28 +1,55 @@
 class Solution {
     // Two Pointers, O(n) time, O(1) space
+    // public boolean isPalindrome(String s) {
+    //     // s = s.toLowerCase().replaceAll("[^a-z0-9]", ""); // regex trick
+    //     int left = 0;
+    //     int right = s.length() - 1;
+
+    //     if (s.length() == 1) return true;
+
+    //     while (left < right) {
+    //         while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+    //             left++;
+    //         }
+    //         while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+    //             right--;
+    //         }
+
+    //         char cL = Character.toLowerCase(s.charAt(left));
+    //         char cR = Character.toLowerCase(s.charAt(right));
+
+    //         if (cL != cR) return false;
+
+    //         left++;
+    //         right--;
+    //     }
+    //     return true;
+    // }
+
+    // Recursive approach O(n) time, O(n) space
     public boolean isPalindrome(String s) {
-        // s = s.toLowerCase().replaceAll("[^a-z0-9]", ""); // regex trick
         int left = 0;
         int right = s.length() - 1;
-
         if (s.length() == 1) return true;
+        return isPalindromeHelper(s, left, right);
+    }
 
-        while (left < right) {
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            }
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
+    private boolean isPalindromeHelper(String s, int left, int right) {
+        if (left >= right) return true;
 
-            char cL = Character.toLowerCase(s.charAt(left));
-            char cR = Character.toLowerCase(s.charAt(right));
-
-            if (cL != cR) return false;
-
-            left++;
-            right--;
+        if (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+            return isPalindromeHelper(s, left + 1, right);
         }
-        return true;
+        if (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+            return isPalindromeHelper(s, left, right - 1);
+        }
+        
+        char cL = Character.toLowerCase(s.charAt(left));
+        char cR = Character.toLowerCase(s.charAt(right));
+
+        if (cL != cR) {
+            return false;
+        }
+        return isPalindromeHelper(s, left + 1, right - 1);
     }
 }
