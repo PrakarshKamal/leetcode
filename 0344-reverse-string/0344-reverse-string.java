f7eaffecdb5c11c20653bb803d0241ad063a1526
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-19 03:05:35 -0400

    Time: 1 ms (21.63%), Space: 51.1 MB (5.02%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -9,16 +9,37 @@ class Solution {
     //         s[i] = stack.pop();
     //     }  
     // }
+
+    // public void reverseString(char[] s) {
+    //     // Two Pointers O(n) time, O(1) space
+    //     if (s.length == 1) return;
+    //     int left = 0;
+    //     int right = s.length - 1;
+    //     while (left < right) {
+    //         char temp = s[left];
+    //         s[left] = s[right];
+    //         s[right] = temp;
+    //         left++;
+    //         right--;
+    //     }
+    // }
+
+    // Recursive, O(n) time, O(n) space
     public void reverseString(char[] s) {
-        // Two Pointers O(n) time, O(1) space
+        if (s.length == 1) return;
         int left = 0;
         int right = s.length - 1;
-        while (left < right) {
+        reverseHelper(s, left, right);
+    }
+
+    private void reverseHelper(char[] s, int left, int right) {
+        if (left >= right) return;
+        if (left < right) {
             char temp = s[left];
             s[left] = s[right];
             s[right] = temp;
-            left++;
-            right--;
+            reverseHelper(s, left + 1, right - 1);
         }
+        return;
     }
 }
