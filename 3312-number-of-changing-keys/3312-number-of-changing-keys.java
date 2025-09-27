fdfc0fdde7bb3bc489ea0d193c3cf57f696d47fc
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-09-27 06:03:28 -0400

    Time: 36 ms (2.23%), Space: 44.5 MB (5.13%) - LeetHub

---
+++ 3312-number-of-changing-keys/3312-number-of-changing-keys.java
@@ -0,0 +1,20 @@
+class Solution {
+    // O(n) time, O(1) space
+    public int countKeyChanges(String s) {
+        if (s.length() == 1) {
+            return 0;
+        }
+        int count = 0;
+        char lastUsed = Character.toLowerCase(s.charAt(0));
+        for (int i = 0; i < s.length(); i++) {
+            char c = Character.toLowerCase(s.charAt(i));
+            System.out.println(c);
+            if (c == lastUsed) {
+                continue;
+            }
+            count++;
+            lastUsed = c;
+        }
+        return count;
+    }
+}
