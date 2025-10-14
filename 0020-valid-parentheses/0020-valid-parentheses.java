5c945ef03aa80bd8c681a0ee62a6ae2a7fcae5a5
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-10-14 02:23:05 -0400

    Time: 3 ms (55.83%), Space: 41.8 MB (71.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,24 @@
 class Solution {
+    // Stack O(n) time, O(n) space
     public boolean isValid(String s) {
+        if (s.length() == 1) return false;
         Stack<Character> stack = new Stack<>();
-        if(s.length() == 1) {
-            return false;
-        }
-        for(char c : s.toCharArray()) {
-            if(c == '(' || c == '[' || c == '{') {
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
             }
             else {
-                if(stack.isEmpty()) {
-                    return false;
-                }
+                if (stack.isEmpty()) return false;
+
                 char top = stack.pop();
+
                 if (c == ')' && top != '(' ||
-                    c == ']' && top != '[' ||
+                    c == ']' && top != '[' || 
                     c == '}' && top != '{') {
-                    return false;
-                }
+                        return false;
+                    }
             }
         }
         return stack.isEmpty();
