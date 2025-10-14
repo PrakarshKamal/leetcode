6126da12a42b8f2670cec8bf8968159d37bec8f4
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-10-14 16:43:49 -0400

    Time: 15 ms (86.37%), Space: 50.5 MB (5.62%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,20 +1,50 @@
 class Solution {
+    // Solution 1, Two Maps O(n) time, O(n) space
+    // public int findShortestSubArray(int[] nums) {
+    //     int degree = 0;
+    //     int minLength = 0;
+    //     Map<Integer, Integer> freqMap = new HashMap<>();
+    //     Map<Integer, Integer> seenMap = new HashMap<>();
+
+    //     for (int i = 0; i < nums.length; i++) {
+    //         seenMap.putIfAbsent(nums[i], i);
+    //         freqMap.put(nums[i], freqMap.getOrDefault(nums[i], 0) + 1);
+
+    //         if (degree < freqMap.get(nums[i])) {
+    //             degree = freqMap.get(nums[i]);
+    //             minLength = i - seenMap.get(nums[i]) + 1; // add one to offset 0-based indexing
+    //         }
+    //         else if (degree == freqMap.get(nums[i])) {
+    //             minLength = Math.min(minLength, i - seenMap.get(nums[i]) + 1);
+    //         }
+    //     }
+    //     return minLength;
+    // }
+
+
     public int findShortestSubArray(int[] nums) {
         int degree = 0;
-        int minLength = 0;
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        Map<Integer, Integer> seenMap = new HashMap<>();
+        int minLength = nums.length;
+        Map<Integer, int[]> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            seenMap.putIfAbsent(nums[i], i);
-            freqMap.put(nums[i], freqMap.getOrDefault(nums[i], 0) + 1);
-
-            if (degree < freqMap.get(nums[i])) {
-                degree = freqMap.get(nums[i]);
-                minLength = i - seenMap.get(nums[i]) + 1;
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], new int[] {i, i, 1});
             }
-            else if (degree == freqMap.get(nums[i])) {
-                minLength = Math.min(minLength, i - seenMap.get(nums[i]) + 1);
+            else {
+                int[] arr = map.get(nums[i]);
+                arr[1] = i;
+                arr[2]++;
+                map.put(nums[i], arr);
+            }
+        }
+        for (int[] value : map.values()) {
+            if (value[2] > degree) {
+                degree = value[2];
+                minLength = value[1] - value[0] + 1;
+            }
+            else if (value[2] == degree) {
+                minLength = Math.min(minLength, value[1] - value[0] + 1);
             }
         }
         return minLength;
