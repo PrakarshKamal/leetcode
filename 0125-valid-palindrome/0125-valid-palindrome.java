ef135f52dbbfbd89075697a022ae6da607371826
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-10-14 00:25:02 -0400

    Time: 14 ms (29.13%), Space: 44.8 MB (38.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,56 +1,47 @@
 class Solution {
     // Two Pointers, O(n) time, O(1) space
-    // public boolean isPalindrome(String s) {
-    //     // s = s.toLowerCase().replaceAll("[^a-z0-9]", ""); // regex trick
-    //     int left = 0;
-    //     int right = s.length() - 1;
-
-    //     if (s.length() == 1) return true;
-
-    //     while (left < right) {
-    //         while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-    //             left++;
-    //         }
-    //         while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-    //             right--;
-    //         }
-
-    //         char cL = Character.toLowerCase(s.charAt(left));
-    //         char cR = Character.toLowerCase(s.charAt(right));
-
-    //         if (cL != cR) return false;
-
-    //         left++;
-    //         right--;
-    //     }
-    //     return true;
-    // }
-
-    // Recursive approach O(n) time, O(n) space
     public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
         int left = 0;
         int right = s.length() - 1;
-        if (s.length() == 1) return true;
-        return isPalindromeHelper(s, left, right);
+
+        while (left < right) {
+            char cL = s.charAt(left);
+            char cR = s.charAt(right);
+
+            if (cL != cR) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
-    private boolean isPalindromeHelper(String s, int left, int right) {
-        // base case, pointers crossed so nothing to check
-        if (left >= right) return true; 
-
-        if (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-            return isPalindromeHelper(s, left + 1, right);
-        }
-        if (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-            return isPalindromeHelper(s, left, right - 1);
-        }
+    // Recursive approach O(n) time, O(n) space
+    // public boolean isPalindrome(String s) {
+    //     s = s.toLowerCase().replaceAll("[^a-z0-9]", ""); // regex trick
+    //     int left = 0;
+    //     int right = s.length() - 1;
         
-        char cL = Character.toLowerCase(s.charAt(left));
-        char cR = Character.toLowerCase(s.charAt(right));
+    //     if (s.length() == 1) return true;
+    //     return isPalindromeHelper(s, left, right);
+    // }
 
-        if (cL != cR) {
-            return false;
-        }
-        return isPalindromeHelper(s, left + 1, right - 1);
-    }
+    // private boolean isPalindromeHelper(String s, int left, int right) {
+    //     // Sytem.out.println(s);
+    //     // base case, pointers crossed so nothing to check
+    //     if (left >= right) {
+    //         return true;
+    //     }
+
+    //     char cL = s.charAt(left);
+    //     char cR = s.charAt(right);
+
+    //     if (cL != cR) {
+    //         return false;
+    //     }
+    //     return isPalindromeHelper(s, left + 1, right - 1);
+    // }
 }
