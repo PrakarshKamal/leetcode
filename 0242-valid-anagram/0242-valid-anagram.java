238f87f239d04b497da48f959caf2b58fe27c9a1
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-12-13 12:26:12 -0500

    Time: 6 ms (43.74%), Space: 44.1 MB (82.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,50 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
         
-        if(s.length() != t.length()) {
-            return false;
-        }
+        int[] ans = new int[26];
 
-        // Solution1 Sorting: O(nlogn + mlogm) time, O(1) space
-        // char[] s1 = s.toCharArray();
-        // Arrays.sort(s1);
-        // String sortedS = new String(s1);
-
-        // char[] t1 = t.toCharArray();
-        // Arrays.sort(t1);
-        // String sortedT = new String(t1);
-
-        // if(sortedS.equals(sortedT)) {
-        //     return true;
-        // }
-        // return false;
-
-
-        // Solution2 Hashing: O(n + m) time, O(1) space
-        // HashMap<Character, Integer> sMap = new HashMap<>();
-        // HashMap<Character, Integer> tMap = new HashMap<>();
-
-        // for(int i = 0; i < s.length(); i++) {
-        //     char sChar = s.charAt(i);
-        //     char tChar = t.charAt(i);
-        //     sMap.put(sChar, sMap.getOrDefault(sChar, 0) + 1);
-        //     tMap.put(tChar, tMap.getOrDefault(tChar, 0) + 1);
-        // }
-        // return sMap.equals(tMap);
-
-        // Solution3 Char trick: O(n + m) time, O(1) space
-        int[] arr = new int[26];
-
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
-
-            arr[sChar - 'a']++;
-            arr[tChar - 'a']--;
+            ans[sChar - 'a']++;
+            ans[tChar - 'a']--;
         }
 
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] != 0) {
+        for (int i = 0; i < ans.length; i++) {
+            if (ans[i] != 0) {
                 return false;
             }
         }
