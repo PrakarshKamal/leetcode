5cb53d09b1177430b3261b7b55acc05c4691ef07
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-12-13 12:29:23 -0500

    Time: 26 ms (6.2%), Space: 46.5 MB (14.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,40 @@
 class Solution {
+    // public boolean isAnagram(String s, String t) {
+    //     if (s.length() != t.length()) return false;
+
+    //     int[] ans = new int[26];
+
+    //     for (int i = 0; i < s.length(); i++) {
+    //         char sChar = s.charAt(i);
+    //         char tChar = t.charAt(i);
+    //         ans[sChar - 'a']++;
+    //         ans[tChar - 'a']--;
+    //     }
+
+    //     for (int i = 0; i < ans.length; i++) {
+    //         if (ans[i] != 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-        
-        int[] ans = new int[26];
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
-            ans[sChar - 'a']++;
-            ans[tChar - 'a']--;
+            sMap.put(sChar, sMap.getOrDefault(sChar, 0)+1);
+            tMap.put(tChar, tMap.getOrDefault(tChar, 0)+1);
         }
 
-        for (int i = 0; i < ans.length; i++) {
-            if (ans[i] != 0) {
-                return false;
-            }
+        if (sMap.equals(tMap)) {
+            return true;
         }
-        return true;
+        return false;
     }
 }
