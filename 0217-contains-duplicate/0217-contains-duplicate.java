f9beaa7c78c94c2a0155b92aa6948239af010f6a
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-12-13 12:11:49 -0500

    Time: 14 ms (78.56%), Space: 93.2 MB (44.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,24 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // O(n) time, O(n) space
-        // Set<Integer> seen = new HashSet<>();
-        // for (int num : nums) {
-        //     if (seen.contains(num)) {
-        //         return true;
-        //     }
-        //     seen.add(num);
-        // }
-        // return false;
-
-
-        // Map approach O(n) timne, O(n) space
-        Map<Integer, Integer> map = new HashMap<>();
-
+        HashSet<Integer> set = new HashSet<>();
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-            if (map.get(num) > 1) {
+            if (set.contains(num)) {
                 return true;
             }
+            set.add(num);
         }
         return false;
     }
