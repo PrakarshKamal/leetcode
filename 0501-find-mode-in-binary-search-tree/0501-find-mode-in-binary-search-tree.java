927336a7ab6f29100d78b2a83f1bf8407904fe57
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-12-17 18:02:25 -0500

    Time: 0 ms (100%), Space: 46.9 MB (40.6%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -13,36 +13,80 @@
  *     }
  * }
  */
+
+// brute force O(n) time, O(n) space
+// class Solution {
+//     public int[] findMode(TreeNode root) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         inorder(root, map);
+
+//         int mode = 0;
+//         for (Integer count : map.values()) {
+//             mode = Math.max(mode, count);
+//         }
+
+//         List<Integer> curr = new ArrayList<>();
+//         for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+//             if (e.getValue() == mode) {
+//                 curr.add(e.getKey());
+//             }
+//         }
+
+//         int[] ans = new int[curr.size()];
+//         for (int i = 0; i < curr.size(); i++) {
+//             ans[i] = curr.get(i);
+//         }
+//         return ans;
+//     }
+
+//     public void inorder(TreeNode root, Map<Integer, Integer> map) {
+//         if (root == null) return;
+
+//         inorder(root.left, map);
+//         map.put(root.val, map.getOrDefault(root.val, 0)+1);
+//         inorder(root.right, map);
+//     }
+// }
+
 class Solution {
+
+    Integer prev = null;
+    int currCount = 0;
+    int maxCount = 0;
+
     public int[] findMode(TreeNode root) {
-        Map<Integer, Integer> map = new HashMap<>();
-        inorder(root, map);
+        List<Integer> modes = new ArrayList<>();
+        inorder(root, modes);
 
-        int mode = 0;
-        for (Integer count : map.values()) {
-            mode = Math.max(mode, count);
-        }
-
-        List<Integer> curr = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            if (e.getValue() == mode) {
-                curr.add(e.getKey());
-            }
-        }
-
-        int[] ans = new int[curr.size()];
-        for (int i = 0; i < curr.size(); i++) {
-            ans[i] = curr.get(i);
+        int[] ans = new int[modes.size()];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = modes.get(i);
         }
         return ans;
-        
     }
 
-    public void inorder(TreeNode root, Map<Integer, Integer> map) {
+    public void inorder(TreeNode root, List<Integer> modes) {
         if (root == null) return;
 
-        inorder(root.left, map);
-        map.put(root.val, map.getOrDefault(root.val, 0)+1);
-        inorder(root.right, map);
+        inorder(root.left, modes);
+
+        if (prev != null && root.val == prev) {
+            currCount++;
+        }
+        else {
+            currCount = 1;
+        }
+
+        if (currCount > maxCount) {
+            maxCount = currCount;
+            modes.clear();
+            modes.add(root.val);
+        }
+        else if (currCount == maxCount) {
+            modes.add(root.val);
+        }
+        prev = root.val;
+
+        inorder(root.right, modes);
     }
 }
