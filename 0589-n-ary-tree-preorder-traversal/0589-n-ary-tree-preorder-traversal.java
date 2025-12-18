d86d8e297b8a1f44ba49f5c1befce8a57b95dc6f
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-12-18 01:05:45 -0500

    Time: 0 ms (100%), Space: 46.5 MB (34.24%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+        helper(root, ans);
+        return ans;
+    }
+    public void helper(Node root, List<Integer> ans) {
+        if (root == null) return;
+        ans.add(root.val); // preorder so add root.val first
+        for (Node child : root.children) {
+            helper(child, ans);
+        }
+    }
+}
