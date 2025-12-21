72a4f5e2362ee2373709a56fce3661bb6ad79d38
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-12-21 00:00:38 -0500

    Time: 1 ms (100%), Space: 47.6 MB (49.9%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
@@ -14,23 +14,56 @@
  * }
  */
 
+// O(n^2) time, O(n) space
+// class Solution {
+//     public List<TreeNode> allPossibleFBT(int n) {
+//         return helper(n);
+//     }
+//     public List<TreeNode> helper(int n) {
+//         List<TreeNode> ans = new ArrayList<>();
+//         TreeNode root = new TreeNode(0);
+//         if (n == 1) { // base case
+//             ans.add(root);
+//             return ans;
+//         }
+//         List<TreeNode> leftNodes;
+//         List<TreeNode> rightNodes;
+//         // left + right should equal n-1 nodes, 1 node for root
+//         for (int i = 1; i < n-1; i+=2) { // increment by 2 since it needs to be odd
+//             leftNodes = helper(i); 
+//             rightNodes = helper(n-1-i);
+
+//             for (TreeNode left : leftNodes) {
+//                 for (TreeNode right : rightNodes) {
+//                     TreeNode node = new TreeNode(0);
+//                     node.left = left;
+//                     node.right = right;
+//                     ans.add(node);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public List<TreeNode> allPossibleFBT(int n) {
-        return helper(n);
+        List<TreeNode>[] dp = new ArrayList[n+1];
+        return helper(n, dp);
     }
-    public List<TreeNode> helper(int n) {
+    public List<TreeNode> helper(int n, List<TreeNode>[] dp) {
         List<TreeNode> ans = new ArrayList<>();
-        TreeNode root = new TreeNode(0);
-        if (n <= 1) { // base case
-            ans.add(root);
+        if (dp[n] != null) return dp[n];
+        if (n == 1) { // base case
+            ans.add(new TreeNode(0));
             return ans;
         }
         List<TreeNode> leftNodes;
         List<TreeNode> rightNodes;
         // left + right should equal n-1 nodes, 1 node for root
         for (int i = 1; i < n-1; i+=2) { // increment by 2 since it needs to be odd
-            leftNodes = helper(i); 
-            rightNodes = helper(n-1-i);
+            leftNodes = helper(i, dp); 
+            rightNodes = helper(n-1-i, dp);
 
             for (TreeNode left : leftNodes) {
                 for (TreeNode right : rightNodes) {
@@ -41,6 +74,7 @@ class Solution {
                 }
             }
         }
-        return ans;
+        dp[n] = ans;
+        return dp[n];
     }
 }
