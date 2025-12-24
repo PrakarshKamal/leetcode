5defc5f7c9dd5b267170fca98f22ff94a5550e75
Prakarsh Kamal <kamalp@mcmaster.ca>
2025-12-24 03:19:16 -0500

    Time: 148 ms (42.6%), Space: 254.1 MB (77.25%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -55,6 +55,33 @@
 // }
 
 // DFS Adjacency List
+// class Solution {
+//     public boolean validPath(int n, int[][] edges, int source, int destination) {
+//         Map<Integer, List<Integer>> graph = new HashMap<>();
+//         boolean[] visited = new boolean[n];
+//         for (int[] edge : edges) {
+//             int u = edge[0];
+//             int v = edge[1];
+//             graph.computeIfAbsent(u, value -> new ArrayList<>()).add(v);
+//             graph.computeIfAbsent(v, value -> new ArrayList<>()).add(u);
+//         }
+//         return dfs(graph, source, destination, visited);
+//     }
+//     public boolean dfs(Map<Integer, List<Integer>> graph, int source, int destination, boolean[] visited) {
+//         if (source == destination) return true; // base case
+//         visited[source] = true;
+//         for (int neighbor : graph.get(source)) {
+//             if (!visited[neighbor]) {
+//                 if (dfs(graph, neighbor, destination, visited)) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+// DFS Adjacency List
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
@@ -65,15 +92,17 @@ class Solution {
             graph.computeIfAbsent(u, value -> new ArrayList<>()).add(v);
             graph.computeIfAbsent(v, value -> new ArrayList<>()).add(u);
         }
-        return dfs(graph, source, destination, visited);
-    }
-    public boolean dfs(Map<Integer, List<Integer>> graph, int source, int destination, boolean[] visited) {
-        if (source == destination) return true;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(source);
         visited[source] = true;
-        for (int neighbor : graph.get(source)) {
-            if (!visited[neighbor]) {
-                if (dfs(graph, neighbor, destination, visited)) {
-                    return true;
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            if (curr == destination) return true;
+            for (int neighbor : graph.get(curr)) {
+                if (!visited[neighbor]) {
+                    q.offer(neighbor);
+                    visited[neighbor] = true;
                 }
             }
         }
