68f82ad0e1502b2d204e67a8fd895918361168d9
Prakarsh Kamal <kamalp@mcmaster.ca>
2026-01-03 06:00:57 -0500

    Time: 27 ms (98.18%), Space: 59.1 MB (59.95%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,17 +1,49 @@
+// Brute force O(n^3) time, O(n) space
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         Set<List<Integer>> set = new HashSet<>();
+//         Arrays.sort(nums);
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i+1; j < nums.length; j++) {
+//                 for (int k = j+1; k < nums.length; k++) {
+//                     if (nums[i] + nums[j] + nums[k] == 0) {
+//                         List<Integer> curr = Arrays.asList(nums[i], nums[j], nums[k]);
+//                         set.add(curr);
+//                     }
+//                 }
+//             }
+//         }
+//         return new ArrayList<>(set);
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                for (int k = j+1; k < nums.length; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        List<Integer> curr = Arrays.asList(nums[i], nums[j], nums[k]);
-                        set.add(curr);
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int left = i+1;
+            int right = nums.length-1;
+            while (left < right) {
+                int threeSum = nums[i] + nums[left] + nums[right];
+                if (threeSum > 0) {
+                    right--;
+                }
+                else if (threeSum < 0) {
+                    left++;
+                }
+                else {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    while (nums[left] == nums[left-1] && left < right) {
+                        left++;
                     }
                 }
             }
         }
-        return new ArrayList<>(set);
+        return ans;
     }
 }
