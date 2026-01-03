e7b36cf4a44dfb4d1acbce8b474ab5f00fe72e10
Prakarsh Kamal <kamalp@mcmaster.ca>
2026-01-03 15:40:12 -0500

    Time: 17 ms (73.58%), Space: 45.4 MB (79.54%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,3 +1,4 @@
+// Two pointers O(n^2) + O(nlogn) time
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int n = nums.length;
@@ -5,21 +6,19 @@ class Solution {
         int bestSum = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
+            // if (i > 0 && nums[i] == nums[i-1]) continue;
             int left = i+1;
             int right = n-1;
-            
+
             while (left < right) {
                 int currSum = nums[i] + nums[left] + nums[right]; 
-                int currDiff = Math.abs(currSum - target);
-                int bestDiff = Math.abs(bestSum - target);
-
-                if (currDiff < bestDiff) {
+            
+                if (Math.abs(currSum-target) < Math.abs(bestSum-target)) {
                     bestSum = currSum;
                 }
 
                 if (currSum == target) {
-                    return currSum; // found so we return
+                    return currSum; // found so we return early
                 }
 
                 else if (currSum < target) {
