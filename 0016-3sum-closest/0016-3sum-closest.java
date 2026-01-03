aff06328b8dccc98ec8155ab2d0334123a23f5b5
Prakarsh Kamal <kamalp@mcmaster.ca>
2026-01-03 15:43:00 -0500

    Time: 790 ms (5.02%), Space: 45.7 MB (12.83%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,4 +1,3 @@
-// Two pointers O(n^2) + O(nlogn) time
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int n = nums.length;
@@ -6,29 +5,50 @@ class Solution {
         int bestSum = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            int left = i+1;
-            int right = n-1;
-
-            while (left < right) {
-                int currSum = nums[i] + nums[left] + nums[right]; 
-            
-                if (Math.abs(currSum-target) < Math.abs(bestSum-target)) {
-                    bestSum = currSum;
-                }
-
-                if (currSum == target) {
-                    return currSum; // found so we return early
-                }
-
-                else if (currSum < target) {
-                    left++;
-                }
-                else {
-                    right--;
+            for (int j = i+1; j < n; j++) {
+                for (int k = j+1; k < n; k++) {
+                    int currSum = nums[i] + nums[j] + nums[k];
+                    if (Math.abs(currSum-target) < Math.abs(bestSum-target)) {
+                        bestSum = currSum;
+                    }
                 }
             }
         }
         return bestSum;
     }
 }
+
+// Two pointers O(n^2) + O(nlogn) time
+// class Solution {
+//     public int threeSumClosest(int[] nums, int target) {
+//         int n = nums.length;
+//         Arrays.sort(nums);
+//         int bestSum = Integer.MAX_VALUE;
+
+//         for (int i = 0; i < n; i++) {
+//             if (i > 0 && nums[i] == nums[i-1]) continue;
+//             int left = i+1;
+//             int right = n-1;
+
+//             while (left < right) {
+//                 int currSum = nums[i] + nums[left] + nums[right]; 
+            
+//                 if (Math.abs(currSum-target) < Math.abs(bestSum-target)) {
+//                     bestSum = currSum;
+//                 }
+
+//                 if (currSum == target) {
+//                     return currSum; // found so we return early
+//                 }
+
+//                 else if (currSum < target) {
+//                     left++;
+//                 }
+//                 else {
+//                     right--;
+//                 }
+//             }
+//         }
+//         return bestSum;
+//     }
+// }
