54f9919536067f863ea8c5b6ad5849b3f5046d8d
Prakarsh Kamal <kamalp@mcmaster.ca>
2026-01-03 15:18:50 -0500

    Time: 34 ms (56.91%), Space: 58.8 MB (91.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -30,18 +30,22 @@ class Solution {
             int right = nums.length-1;
             while (left < right) {
                 int threeSum = nums[i] + nums[left] + nums[right];
-                if (threeSum > 0) {
+                if (threeSum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left+1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right-1]) {
+                        right--;
+                    }
+                    left++;
                     right--;
                 }
                 else if (threeSum < 0) {
                     left++;
                 }
                 else {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    while (nums[left] == nums[left-1] && left < right) {
-                        left++;
-                    }
+                    right--;
                 }
             }
         }
