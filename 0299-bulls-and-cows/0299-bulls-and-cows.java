169e40b06f71fb8794964f550d6073d323d3ebdb
Prakarsh Kamal <kamalp@mcmaster.ca>
2026-01-04 04:28:49 -0500

    Time: 1 ms (100%), Space: 43.8 MB (37%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.java
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -2,7 +2,8 @@ class Solution {
     public String getHint(String secret, String guess) {
         int n = secret.length();
         StringBuilder sb = new StringBuilder();
-        Map<Character, Integer> map = new HashMap<>();
+        // Map<Character, Integer> map = new HashMap<>();
+        int[] arr = new int[10];
         int bullCount = 0;
         int cowCount = 0;
         for (int i = 0; i < n; i++) {
@@ -12,17 +13,19 @@ class Solution {
                 bullCount++;
             }
             else {
-                map.put(s, map.getOrDefault(s, 0) + 1);
+                // map.put(s, map.getOrDefault(s, 0) + 1);
+                arr[s - '0']++;
             }
         }
         for (int i = 0; i < n; i++) {
             char s = secret.charAt(i);
             char g = guess.charAt(i);
             if (s != g) {
-                int freq = map.getOrDefault(g, 0);
-                if (freq > 0) {
+                // int freq = map.getOrDefault(g, 0);
+                if (arr[g - '0'] > 0) {
                     cowCount++;
-                    map.put(g, freq-1);
+                    // map.put(g, freq-1);
+                    arr[g - '0']--;
                 }
             }
         }
