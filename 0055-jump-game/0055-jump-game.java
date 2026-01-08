ec877bfb38dae5949c2a6d658f3ac54fbd724455
Prakarsh Kamal <kamalp@mcmaster.ca>
2026-01-08 11:29:08 -0500

    Time: 1 ms (99.87%), Space: 47.5 MB (96.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,24 @@
+// O(n) time, O(1) space
 class Solution {
+    // public boolean canJump(int[] nums) {
+    //     int n = nums.length;
+    //     int canReach = 0;
+    //     for (int i = 0; i < n; i++) {
+    //         if (i > canReach) { // to check even if is possible to reach
+    //             return false;
+    //         }
+    //         canReach = Math.max(canReach, i + nums[i]);
+    //     }
+    //     return canReach >= n-1;
+    // }
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        int canReach = 0;
-        for (int i = 0; i < n; i++) {
-            if (i > canReach) { // to check even if is possible to reach
-                return false;
+        int canReach = n-1;
+        for (int i = n-2; i >= 0; i--) {
+            if (i + nums[i] >= canReach) { // keep moving left if can
+                canReach = i;
             }
-            canReach = Math.max(canReach, i + nums[i]);
         }
-        return canReach >= n-1;
+        return canReach == 0;
     }
 }
