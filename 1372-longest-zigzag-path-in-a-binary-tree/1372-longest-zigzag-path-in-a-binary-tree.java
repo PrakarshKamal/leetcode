c02d44c3e27455e532f28cd928c6299c698b1103
Prakarsh Kamal <kamalp@mcmaster.ca>
2026-01-10 05:07:50 -0500

    Time: 7 ms (26.53%), Space: 57.5 MB (82.43%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+ // DFS O(n) time, O(n) space
 class Solution {
     int ans = 0;
     public int longestZigZag(TreeNode root) {
@@ -23,6 +24,8 @@ class Solution {
     public void dfs(TreeNode root, boolean isDirLeft, int length) {
         if (root == null) return;
 
+        ans = Math.max(ans, length);
+
         if (isDirLeft) {
             dfs(root.left, false, length+1);
             dfs(root.right, true, 1);
@@ -31,6 +34,5 @@ class Solution {
             dfs(root.right, true, length+1);
             dfs(root.left, false, 1);
         }
-        ans = Math.max(ans, length);
     }
 }
