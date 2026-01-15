c3bc851036f2f40bc8f04df73f5c35f90e6ed6fd
Prakarsh Kamal <kamalp@mcmaster.ca>
2026-01-15 03:21:47 -0500

    Time: 0 ms (100%), Space: 43.4 MB (9.95%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int space = s.lastIndexOf(' ');
+        String word = s.substring(space+1, s.length());
+
+        return word.length();
+    }
+}
