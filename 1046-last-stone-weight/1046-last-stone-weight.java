7d8d42ed7c105ab7a2bfb49cc63c10d1f7c79286
Prakarsh Kamal <kamalp@mcmaster.ca>
2026-01-15 23:53:08 -0500

    Time: 1 ms (99.35%), Space: 42.9 MB (36.72%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,50 @@
+class Solution {
+    // Brute force sorting approach, O(n^2 log n) time, O(n) space
+    // public int lastStoneWeight(int[] stones) {
+    //     List<Integer> arr = new ArrayList<>();
+    //     for (int stone : stones) {
+    //         arr.add(stone);
+    //     }
+
+    //     while (arr.size() > 1) {
+    //         Collections.sort(arr, Collections.reverseOrder());
+
+    //         int y = arr.remove(0); // largest
+    //         int x = arr.remove(0); // next largest
+    //         int curr = y - x;
+
+    //         if (curr != 0) {
+    //             arr.add(curr);
+    //         }
+    //     }
+        
+    //     if (arr.isEmpty()) {
+    //         return 0;
+    //     }
+    //     return arr.get(0);
+    // }
+
+    public int lastStoneWeight(int[] stones) {
+        // Max heap approach, O(n log n) time, O(n) space
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+        while (maxHeap.size() >= 2) {
+            int y = maxHeap.poll(); // largest
+            int x = maxHeap.poll(); // next largest
+            int curr = y - x;
+
+            if (curr != 0) {
+                maxHeap.offer(curr);
+            }
+        }
+
+        if (maxHeap.isEmpty()) {
+            return 0;
+        }
+        return maxHeap.peek();
+    }
+}
